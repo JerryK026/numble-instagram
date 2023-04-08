0919b8116040257a69550324f301da2bef0a2f49
SeokyungKim <kskyung0624@gmail.com>
2023-04-08 18:21:44 +0900

    test : Post 생성 기능에 대한 인수 테스트를 작성한다

---
+++ src/test/kotlin/com/numble/instagram/post/acceptance/PostAcceptanceTest.kt
@@ -0,0 +1,36 @@
+package com.numble.instagram.post.acceptance
+
+import com.numble.instagram.domain.post.application.PostCreateFacade
+import com.numble.instagram.domain.post.dto.request.PostCreateRequest
+import com.numble.instagram.global.fakeMultipartFileUrlFactory
+import com.numble.instagram.infra.FakeMultipartFile
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostAcceptanceTest @Autowired constructor(
+    private val postCreateFacade: PostCreateFacade,
+) {
+
+    @Test
+    @DisplayName("게시물을 저장에 성공한다")
+    fun postCreate() {
+        // given
+        val FILENAME = "fileName"
+        val CONTENT = "content"
+
+        // when
+        val response = postCreateFacade.create(
+            PostCreateRequest(
+                FakeMultipartFile(FILENAME.toByteArray(), FILENAME), CONTENT)
+        )
+
+        // then
+        assertThat(response.id).isNotNull()
+        assertThat(response.content).isEqualTo(CONTENT)
+        assertThat(response.imageUrl).isEqualTo(fakeMultipartFileUrlFactory(FILENAME))
+    }
+}
