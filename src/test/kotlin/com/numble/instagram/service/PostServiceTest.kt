75dd818b565f2f7739bd14c8811e1f39a0f83067
SeokyungKim <kskyung0624@gmail.com>
2023-04-08 18:08:05 +0900

    test : Post 생성 테스트를 작성한다

---
+++ src/test/kotlin/com/numble/instagram/service/PostServiceTest.kt
@@ -0,0 +1,29 @@
+package com.numble.instagram.service
+
+import com.numble.instagram.domain.post.domain.Post
+import com.numble.instagram.domain.post.dao.PostRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostServiceTest @Autowired constructor(
+//    private val postService: PostService,
+    private val postRepository: PostRepository,
+) {
+
+    @Test
+    @DisplayName("게시물을 저장에 성공한다")
+    fun savePostTest() {
+        // given
+        postRepository.save(Post("content", "imageUrl"))
+
+        // when & then
+        val posts = postRepository.findAll()
+        assertThat(posts).hasSize(1)
+        assertThat(posts[0].content).isEqualTo("content")
+        assertThat(posts[0].imageUrl).isEqualTo("imageUrl")
+    }
+}
